4a52dce3cdfeeabefdadb37521c27a702de39e0c
GabrielePisapia <50744224+GabrielePisapia@users.noreply.github.com>
2020-11-18 11:55:42 +0100

    Update tripadvisorSQL.sql

--- fabrikam-functions/file_list/tripadvisorSQL.sql
+++ fabrikam-functions/file_list/tripadvisorSQL.sql
@@ -2,12 +2,11 @@ create schema tripadvisor;
 use tripadvisor;
 
 create table Ristorante (
-	idRistorante int not null auto_increment,
     Nome varchar(100) not null,
     Citta varchar(50) not null,
     Cucina varchar(4000) not null,
     Rate double,
-    primary key (idRistorante)
+    primary key (Nome)
 );
 
 create table Recensione (
@@ -15,10 +14,10 @@ create table Recensione (
     Corpo varchar(5000) not null,
     UserLocation varchar(300) not null,
     Ratereview double,
-	RistoranteId int not null,
-    foreign key (RistoranteId)
-    references tripadvisor.Ristorante(idRistorante)
+	NomeRistorante varchar(100) not null,
+    foreign key (NomeRistorante)
+    references tripadvisor.Ristorante(Nome)
     on delete no action
     on update no action,
-    primary key (RistoranteId)
+    primary key (NomeRistorante)
 );
